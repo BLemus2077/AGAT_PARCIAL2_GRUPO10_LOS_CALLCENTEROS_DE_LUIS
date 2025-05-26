21e669d6e7e880fddc0e734f97daeada0098e976
se21003 <se21003@ues.edu.sv>
2025-05-25 19:41:38 -0600

    Ajusta redirección por roles y mejora manejo de excepciones

---
+++ src/main/java/com/libcode/crud/config/GrantedAuthoritiesConverter.java
@@ -0,0 +1,29 @@
+package com.libcode.crud.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    private static final String ROLES_CLAIM = "https://agat.app/roles"; // Asegúrate de que este sea el usado en tu Action
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        List<String> roles = jwt.getClaimAsStringList(ROLES_CLAIM);
+
+        if (roles == null || roles.isEmpty()) {
+            return List.of(); // No roles = sin privilegios
+        }
+
+        return roles.stream()
+                .map(role -> "ROLE_" + role.toUpperCase()) // Spring espera "ROLE_..."
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
